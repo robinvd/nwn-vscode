7cbb838e345c1c9bbb2c471b6b37ff2cc4c3967f
Robin van Dijk <robin@robinjint.nl>
2021-01-17 16:27:41 +0100

    init

---
+++ client/src/extension.ts
@@ -0,0 +1,50 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	let serverOptions: ServerOptions = {
+		command: "/home/robin.local/Documents/thesis/lsp/target/debug/lsp"
+	}
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: ["*"],
+		// synchronize: {
+		// Notify the server about file changes to '.clientrc files contained in the workspace
+		// fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		// }
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
